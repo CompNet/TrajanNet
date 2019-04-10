5542999380310ff50822141accd22b155e36a8f1
Vincent Labatut <vince.labatut@gmail.com>
2019-04-10 12:13:53 +0200

    added new layout for ego-graphs

--- src/main.R
+++ src/main.R
@@ -9,6 +9,8 @@
 #############################################################################################
 # load libraries
 library("igraph")
+library("graphlayouts")
+library("ggraph")
 
 # load other scripts
 source("src/extraction.R")
@@ -22,6 +24,20 @@ net.folder <- file.path(data.folder,"nets")
 
 # extract network
 g <- extract.network()
+
+# plot network
+p1 <- ggraph(g, layout="focus", v=1) +
+		draw_circle(use = "focus", max.circle = 3)+
+		geom_edge_link(edge_color="black",edge_width=0.3)+
+#		geom_node_point(aes(fill=as.factor(Faction)),size=2,shape=21)+
+		scale_fill_manual(values=c("#8B2323", "#EEAD0E"))+
+		theme_graph()+
+		theme(legend.position = "none")+
+		coord_fixed()+
+		labs(title= "Trajan's ego-network")
+# https://cran.r-project.org/web/packages/graphlayouts/vignettes/introduction.html
+# https://cran.r-project.org/web/packages/graphlayouts/index.html
+# https://cran.r-project.org/web/packages/ggraph/index.html
 
 
 # analyze network
