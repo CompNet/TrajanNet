cf7d50fe9ac628360956e53fc57b0f3b6a7ae416
Vincent Labatut <vince.labatut@gmail.com>
2019-09-10 18:22:50 +0200

    updated plot script

--- src/analysis.R
+++ src/analysis.R
@@ -9,20 +9,25 @@
 #############################################################################################
 
 #############################################################
-# xxx
+# Displays the specified graph in an appropraite way, taking
+# into account the previously set link and node attributes.
 #
-# xxx:xxx.
+# g: graph to plot.
+# paths: (optional) paths to highlight while plotting. This parameter
+# 		 is either a list of integer vectors (node sequences), or
+# 		 an integer vector if there is only one path to plot.
+# file: (optional) file name, to record the plot.
 #############################################################
-myplot <- function(g, paths)
+myplot <- function(g, paths, file)
 {	
 	# set edge colors
 	ecols <- rep("BLACK", gsize(g))
-	ecols[E(g)$Nature_Amicale] <- "#1a8f39"			# green
-	ecols[E(g)$Nature_Familiale] <- "#9c1699"		# purple
-	ecols[E(g)$Nature_Professionnelle] <- "#c27604"	# orange
+	ecols[E(g)$Type=="Nature_Amicale"] <- "#1a8f39"			# green
+	ecols[E(g)$Type=="Nature_Familiale"] <- "#9c1699"		# purple
+	ecols[E(g)$Type=="Nature_Professionnelle"] <- "#c27604"	# orange
 	# set edge style
-	elty <- 1										# solid
-	elty[E(g)$Polarité=="Négative"] <- 3			# dotted
+	elty <- rep(1,gsize(g))										# solid
+	elty[!is.na(E(g)$"Polarité") & E(g)$"Polarité"=="Négative"] <- 3			# dotted
 	# set edge width
 	ewidth <- rep(1,gsize(g))
 	
@@ -46,6 +51,8 @@ myplot <- function(g, paths)
 		}
 	}
 	
+	if(hasArg(file))
+		pdf(file, width=25, height=25)
 	plot(g,
 		layout=lay,
 		vertex.size=5, 
@@ -54,8 +61,40 @@ myplot <- function(g, paths)
 		edge.lty=elty,
 		edge.width=ewidth
 	)
+	if(hasArg(file))
+		dev.off()
 }
 
+
+
+
+#############################################################
+# Transform the received graph so that instead of having multitype
+# links, each type occurrence is represented by a distinct link.
+# Parameter link.types lists the types to retain in the result
+# graph.
+#
+# g : original graph, whose links will be cleaned to get the result graph.
+# link:types : list of link types to keep in the result graph.
+#############################################################
+clean.links <- function(g, link.types)
+{	res <- delete_edges(g, edges=E(g))
+	for(ea in list.edge.attributes(g))
+		res <- delete_edge_attr(res, ea)
+	
+	for(e in E(g))
+	{	for(lt in link.types)
+		{	if(get.edge.attribute(g,lt,e))
+			{	uv = ends(g, E(g)[e], names=FALSE)
+				res <- add_edges(res, edges=uv, attr=list(
+								"Polarité"=get.edge.attribute(g,lt,e),
+								"Type"=lt))
+			}
+		}
+	}
+	
+	return(res)
+}
 
 
 
@@ -71,7 +110,8 @@ analyze.network <- function(g)
 
 
 # read the graph
-g <- read.graph("data/nets/all.graphml",format="graphml")
+graph.file <- file.path(net.folder,"all.graphml")
+g <- read.graph(graph.file,format="graphml")
 
 # tentative of using predefined layouts
 #lay <- layout_with_fr(g)
@@ -84,7 +124,13 @@ g <- read.graph("data/nets/all.graphml",format="graphml")
 
 # read the layout and plot
 lay <- as.matrix(read.table(file="data/nets/all_layout.txt"))
+myplot(g, file=file.path(net.folder,"all.pdf"))
 myplot(g)
+
+# only keep certain types of links / possibly duplicate certain links
+g <- clean.links(g, link.types=c("Nature_Amicale",
+				"Nature_Familiale",
+				"Nature_Professionnelle"))	# Nature_Amicale Nature_Familiale Nature_Professionnelle
 
 # delete trajan's links for better visibility
 # TODO maybe better to just draw them using a light color?
@@ -92,7 +138,8 @@ myplot(g)
 neis <- as.integer(ego(graph=g,nodes=1,mindist=1)[[1]])
 es <- c(rbind(rep(1,length(neis)), neis))
 g0 <- delete.edges(g,es)	# delete traj's links
-myplot(g0)
+myplot(g0, file=file.path(net.folder,"all_0.pdf"))
+myplot(g0) # TODO pb: ne supprime pas tous les liens !
 
 # work on the diameter
 diam <- diameter(g0)					# get the network diameter
