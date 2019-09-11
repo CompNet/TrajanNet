3f901cd55f4debc32dcf697734cf09b65f50b679
Vincent Labatut <vince.labatut@gmail.com>
2019-09-11 22:53:18 +0200

    updated data, a bit the scripts too

--- src/analysis.R
+++ src/analysis.R
@@ -8,6 +8,9 @@
 # source("src/analysis.R")
 #############################################################################################
 
+
+
+
 #############################################################
 # Displays the specified graph in an appropraite way, taking
 # into account the previously set link and node attributes.
@@ -104,66 +107,79 @@ clean.links <- function(g, link.types)
 
 
 
+
+#############################################################
+# xxx
+#
+# xxx:xxx.
+#############################################################
+analyze.net.diameter <- function(g)
+{	# work on the diameter
+	diam <- diameter(g)					# get the network diameter
+	dd <- distances(graph=g)				# compute all inter-node distances
+	idx <- which(dd==diam, arr.ind=TRUE)	# retrieve pairs of node matching the diameter 
+	idx <- idx[idx[,1]<idx[,2],]			# filter (each pair appears twice due to symmetric matrix)
+	# plot diameter
+	diameter.folder <- file.path(net.folder,"diameter")
+	dir.create(path=diameter.folder, showWarnings=FALSE, recursive=TRUE)
+	diam.paths <- lapply(1:nrow(idx), function(r) all_shortest_paths(graph=g, from=idx[r,1], to=idx[r,2])$res)
+	for(pp in 1:length(diam.paths))
+	{	for(p in 1:length(diam.paths[[pp]]))
+			myplot(g, diam.paths[[pp]][[p]], file=file.path(diameter.folder,paste0("all0_diam_",pp,"_",p,".pdf")))
+		myplot(g, diam.paths[[pp]], file=file.path(diameter.folder,paste0("all0_diam_",pp,".pdf")))
+	}
+}
+
+
+
+
 #############################################################
 # xxx
 #
 # xxx:xxx.
 #############################################################
 analyze.network <- function(g)
-{	
+{	# read the original graph
+	graph.file <- file.path(net.folder,"all.graphml")
+	g <- read.graph(graph.file,format="graphml")
 	
+	# tentative of using predefined layouts
+#	lay <- layout_with_fr(g)
+#	lay <- layout_with_fr(g, kkconst=0)
+	
+	# old code used to setup the layout
+#	tkplot(g)
+#	coord <- tk_coords(3)
+#	write.table(x=coord,file="data/nets/all_layout.txt")
+	
+	# only keep certain types of links / possibly duplicate certain links
+	g <- clean.links(g, link.types=c("Nature_Amicale",
+					"Nature_Familiale",
+					"Nature_Professionnelle"))	# Nature_Amicale Nature_Familiale Nature_Professionnelle
+	
+	# read the layout and plot full graph
+	lay <- as.matrix(read.table(file="data/nets/all_layout.txt"))
+	myplot(g, file=file.path(net.folder,"all.pdf"))
+	myplot(g)
+	
+	# delete trajan's links for better visibility
+	# TODO maybe better to just draw them using a light color?
+	g0 <- g
+#	g0 <- delete_vertices(g,1)		# delete trajan (not good, plot-wise)
+	es <- incident(graph=g0, v=1, mode="all")
+	g0 <- delete.edges(g0,es)	# delete traj's links
+	myplot(g0, file=file.path(net.folder,"all0.pdf"))
+	myplot(g0)
+	
+	# plot diameters
+	analyze.net.diameter(g0)
 }
 
 
-# read the graph
-graph.file <- file.path(net.folder,"all.graphml")
-g <- read.graph(graph.file,format="graphml")
 
-# tentative of using predefined layouts
-#lay <- layout_with_fr(g)
-#lay <- layout_with_fr(g, kkconst=0)
-
-# old code used to setup the layout
-#tkplot(g)
-#coord <- tk_coords(3)
-#write.table(x=coord,file="data/nets/all_layout.txt")
-
-# only keep certain types of links / possibly duplicate certain links
-g <- clean.links(g, link.types=c("Nature_Amicale",
-				"Nature_Familiale",
-				"Nature_Professionnelle"))	# Nature_Amicale Nature_Familiale Nature_Professionnelle
-
-# read the layout and plot
-lay <- as.matrix(read.table(file="data/nets/all_layout.txt"))
-myplot(g, file=file.path(net.folder,"all.pdf"))
-myplot(g)
-
-# delete trajan's links for better visibility
-# TODO maybe better to just draw them using a light color?
-g0 <- g
-#g0 <- delete_vertices(g,1)		# delete trajan
-es <- incident(graph=g0, v=1, mode="all")
-g0 <- delete.edges(g0,es)	# delete traj's links
-myplot(g0, file=file.path(net.folder,"all0.pdf"))
-myplot(g0)
-
-# work on the diameter
-diam <- diameter(g0)					# get the network diameter
-dd <- distances(graph=g0)				# compute all inter-node distances
-idx <- which(dd==diam, arr.ind=TRUE)	# retrieve pairs of node matching the diameter 
-idx <- idx[idx[,1]<idx[,2],]			# filter (each pair appears twice due to symmetric matrix)
-# plot diameter
-diameter.folder <- file.path(net.folder,"diameter")
-dir.create(path=diameter.folder, showWarnings=FALSE, recursive=TRUE)
-diam.paths <- lapply(1:nrow(idx), function(r) all_shortest_paths(graph=g0, from=idx[r,1], to=idx[r,2])$res)
-for(pp in 1:length(diam.paths))
-{	for(p in 1:length(diam.paths[[pp]]))
-		myplot(g0, diam.paths[[pp]][[p]], file=file.path(diameter.folder,paste0("all0_diam_",pp,"_",p,".pdf")))
-	myplot(g0, diam.paths[[pp]], file=file.path(diameter.folder,paste0("all0_diam_",pp,".pdf")))
-}
 
 # TODO
-# prétraitement
-# - fonction qui extrait un graphe avec un seul type de lien
-# - même chose mais avec plusieurs types, avec liens multiples pour qu'ils apparaissent séparément dans les graphiques
-# analyse : regarder plutot l'excentricité ?
+# loop over all types of links in the main function (as well as with/without Trajan)
+# maybe extract them in the main.R file instead, pass them to the analysis script 
+# setup folders for each type of link+all
+# analysis: focus on eccentricity rather than diameter?
